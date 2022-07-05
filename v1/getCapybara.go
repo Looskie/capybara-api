895b1920a0da8abde85987df2f4df4f0491eca06
Cody Miller <50378828+Looskie@users.noreply.github.com>
2022-07-05 15:17:55 -0400

    feat: offer json as a query param annd send the actual imagae
    "thanks hexiro for the idea"
    
    Co-Authored-By: Hexiro <42787085+hexiro@users.noreply.github.com>

--- v1/getCapybara.go
+++ v1/getCapybara.go
@@ -2,7 +2,11 @@ package v1
 
 import (
 	"encoding/json"
+	"errors"
+	"io"
+	"io/ioutil"
 	"math/rand"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 	splash "github.com/hbagdi/go-unsplash/unsplash"
@@ -11,6 +15,7 @@ import (
 
 func GetCapybara(c *fiber.Ctx) error {
 	var unsplash = utils.Unsplash()
+	var wantsJSON = c.Query("json")
 
 	var randomCapys *splash.PhotoSearchResult
 	stringedPhotos, err := utils.RedisGet("random")
@@ -45,8 +50,41 @@ func GetCapybara(c *fiber.Ctx) error {
 
 	randomIndex := rand.Intn(len(*randomCapys.Results))
 
-	return c.JSON(Response{
-		Success: true,
-		Data:    (*randomCapys.Results)[randomIndex],
-	})
+	if wantsJSON == "true" {
+		return c.JSON(Response{
+			Success: true,
+			Data:    (*randomCapys.Results)[randomIndex],
+		})
+	}
+
+	bytes, err := downloadFile((*randomCapys.Results)[randomIndex].Urls.Regular.String())
+
+	if err != nil {
+		return c.JSON(Response{
+			Success: false,
+			Message: err.Error(),
+		})
+	}
+
+	body, err := ioutil.ReadAll(bytes)
+
+	if err != nil {
+		return c.SendString("An error occurred")
+	}
+
+	return c.Send(body)
+}
+
+func downloadFile(URL string) (io.ReadCloser, error) {
+	//Get the response bytes from the url
+	response, err := http.Get(URL)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.StatusCode != 200 {
+		return nil, errors.New("Received non 200 response code")
+	}
+
+	return response.Body, nil
 }
