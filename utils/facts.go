bfee95d5685f72078cbec58d3b3809c46a800b48
slmn <56459644+slammon@users.noreply.github.com>
2023-05-23 13:27:04 -0400

    Added more capybara facts

--- utils/facts.go
+++ utils/facts.go
@@ -35,117 +35,45 @@ var (
 		"Capybaras are known to be intelligent animals and are able to learn from experience.",
 		"They have been observed using tools, such as sticks, to aid in feeding.",
 		"Capybaras are often hunted for their meat and hides, which are used for food and leather.",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"",
+		"Capybaras have a unique grooming behavior called allogrooming, where they mutually groom each other, strengthening social bonds within the group.",
+		"They have specialized jaw muscles that allow them to graze on tough vegetation.",
+		"Capybaras communicate through various vocalizations, including whistles, clicks, and chirps.",
+		"They have a scent gland on their nose called the morrillo, which they use to mark their territory.",
+		"Capybaras have a highly developed sense of hearing and can detect low-frequency sounds.",
+		"They have adapted to a semi-aquatic lifestyle with eyes, ears, and nostrils positioned on top of their head, allowing them to stay partially submerged in water while keeping a lookout for predators.",
+		"Capybaras have sharp incisor teeth that continuously grow throughout their lives.",
+		"They can reach speeds of up to 35 kilometers per hour (22 miles per hour) on land.",
+		"Capybaras engage in playful behavior, often chasing each other and rolling in the mud.",
+		"They have a flexible nose that can close underwater to prevent water from entering their nasal passages.",
+		"Capybaras have a relatively low body temperature, ranging from 36.5 to 38.3 degrees Celsius (97.7 to 100.9 degrees Fahrenheit).",
+		"They have a complex social hierarchy within their groups, with individuals establishing dominance through various behaviors.",
+		"Capybaras have a large cecum in their digestive system, aiding in the fermentation of cellulose and extraction of nutrients.",
+		"They have slightly webbed toes, which assist in swimming and navigating marshy habitats.",
+		"Capybaras have a high tolerance for temperature extremes, adapting well to both hot and cold environments.",
+		"They can produce vocalizations that sound like a purring cat when they are content.",
+		"Capybaras have a strong maternal instinct, with females providing care and protection for their young.",
+		"They have a long gestation period of around 150 days before giving birth.",
+		"Capybaras have a tapetum lucidum, a reflective layer behind their retina, which enhances their night vision.",
+		"They have a relatively low metabolic rate, allowing them to conserve energy in their herbivorous lifestyle.",
+		"Capybaras have slightly flattened heads, which help them stay submerged while keeping their eyes, ears, and nostrils above water.",
+		"They have a long and flexible muzzle that enables them to reach vegetation in the water easily.",
+		"Capybaras have a specialized digestive system that allows them to extract nutrients from tough and fibrous plants efficiently.",
+		"They have specialized teeth called hypsodont teeth, which continue to grow throughout their lives to compensate for the wear caused by their herbivorous diet.",
+		"Capybaras have a strong sense of social bonding and will often engage in nose-to-nose touching as a form of greeting and communication.",
+		"They have a distinctive scent that can be described as musky and is used for communication within their social group.",
+		"Capybaras have a thick layer of fat under their skin that acts as insulation, helping them regulate body temperature in different environments.",
+		"They are excellent at camouflaging themselves in their surroundings, blending in with vegetation to avoid detection by predators.",
+		"Capybaras have a well-developed memory and can remember the location of food sources and safe areas.",
+		"They have long, sturdy claws on their front feet that aid in digging burrows and manipulating food.",
+		"Capybaras have a broad and flat muzzle, which allows them to graze efficiently on grass and other low-lying vegetation.",
+		"They have a strong bite force and can crush hard-shelled fruits and vegetation.",
+		"Capybaras have large, sensitive nostrils that can be closed voluntarily to prevent water from entering while they are submerged.",
+		"They have a keen sense of touch and use their whiskers to detect vibrations and movements in the water.",
+		"Capybaras are known to engage in riding behavior, where smaller animals like birds or turtles may perch on their backs.",
+		"They have a unique mating behavior called coprophagy, where males eat the feces of females as a form of courtship.",
+		"Capybaras have a long and slender body, which allows them to move through dense vegetation with ease.",
+		"They have a strong maternal instinct, and mothers will defend their young fiercely against any potential threats.",
+		"Capybaras have a wide range of facial expressions, including raising their eyebrows and flattening their ears to communicate different emotions.",
+		"They have a highly developed sense of touch in their snout and lips, which helps them identify and select their food.",
 	}
 )
-Capybaras are the largest rodents in the world.
-They are semi-aquatic animals and are excellent swimmers.
-Native to South America, capybaras can be found in countries such as Brazil, Venezuela, Colombia, and Paraguay.
-Capybaras are social animals and live in groups of up to 20 individuals, although larger groups have been observed.
-Groups of capybaras are led by a dominant male, who protects the group and helps to raise the young.
-They have webbed feet, which makes them excellent swimmers.
-They are also able to hold their breath for up to five minutes.
-Capybaras are herbivores and feed mainly on grasses and aquatic plants.
-They have a unique digestive system that allows them to extract nutrients from tough plants.
-Capybaras have a lifespan of around 8-10 years in the wild and up to 12 years in captivity.
-Female capybaras give birth to litters of around 4-5 young, known as pups.
-Pups are born fully furred and with their eyes open, and are able to walk within a few hours.
-Capybaras have a thick, coarse coat that helps to protect them from the cold.
-Their fur is highly prized and is used in the production of luxury goods.
-Capybaras are preyed upon by a variety of predators, including jaguars, anacondas, and caimans.
-They use their keen senses of smell and hearing to detect potential danger.
-Capybaras are highly adaptable animals and can thrive in a variety of habitats, including wetlands, grasslands, and forests.
-They are known to be excellent swimmers and have been observed diving up to 5 meters (16 feet) deep.
-Capybaras are sometimes kept as pets, although this is illegal in many countries.
-They are also used for their meat and their hides, which are used to make leather.
-Capybaras are very social animals and are known to engage in a variety of behaviors, including grooming, playing, and vocalizing.
-They have a variety of vocalizations, including barks, whistles, and purrs.
-Capybaras are known to form close bonds with other members of their group and will often groom each other.
-They are also known to engage in alloparenting, where females will take care of each other's young.
-Capybaras are sometimes called 'water pigs' because of their love of water.
-They have been observed soaking in hot springs to regulate their body temperature.
-They are able to ferment cellulose in their hindgut, which allows them to break down tough plant material.
-Capybaras are able to conserve water by producing dry feces and reabsorbing water from their bladder.
-They are also able to survive without access to fresh water for long periods of time.
-Capybaras are known to be intelligent animals and are able to learn from experience.
-They have been observed using tools, such as sticks, to aid in feeding.
-Capybaras are often hunted for their meat and hides, which are used for food and leather.
-Capybaras have a unique grooming behavior called "allogrooming," where they mutually groom each other, strengthening social bonds within the group.
-They have specialized jaw muscles that allow them to graze on tough vegetation.
-Capybaras communicate through various vocalizations, including whistles, clicks, and chirps.
-They have a scent gland on their nose called the morrillo, which they use to mark their territory.
-Capybaras have a highly developed sense of hearing and can detect low-frequency sounds.
-They have adapted to a semi-aquatic lifestyle with eyes, ears, and nostrils positioned on top of their head, allowing them to stay partially submerged in water while keeping a lookout for predators.
-Capybaras have sharp incisor teeth that continuously grow throughout their lives.
-They can reach speeds of up to 35 kilometers per hour (22 miles per hour) on land.
-Capybaras engage in playful behavior, often chasing each other and rolling in the mud.
-They have a flexible nose that can close underwater to prevent water from entering their nasal passages.
-Capybaras have a relatively low body temperature, ranging from 36.5 to 38.3 degrees Celsius (97.7 to 100.9 degrees Fahrenheit).
-They have a complex social hierarchy within their groups, with individuals establishing dominance through various behaviors.
-Capybaras have a large cecum in their digestive system, aiding in the fermentation of cellulose and extraction of nutrients.
-They have slightly webbed toes, which assist in swimming and navigating marshy habitats.
-Capybaras have a high tolerance for temperature extremes, adapting well to both hot and cold environments.
-They can produce vocalizations that sound like a purring cat when they are content.
-Capybaras have a strong maternal instinct, with females providing care and protection for their young.
-They have a long gestation period of around 150 days before giving birth.
-Capybaras have a tapetum lucidum, a reflective layer behind their retina, which enhances their night vision.
-They have a relatively low metabolic rate, allowing them to conserve energy in their herbivorous lifestyle.
-Capybaras have slightly flattened heads, which help them stay submerged while keeping their eyes, ears, and nostrils above water.
-They have a long and flexible muzzle that enables them to reach vegetation in the water easily.
-Capybaras have a specialized digestive system that allows them to extract nutrients from tough and fibrous plants efficiently.
-They have specialized teeth called hypsodont teeth, which continue to grow throughout their lives to compensate for the wear caused by their herbivorous diet.
-Capybaras have a strong sense of social bonding and will often engage in nose-to-nose touching as a form of greeting and communication.
-They have a distinctive scent that can be described as musky and is used for communication within their social group.
-Capybaras have a thick layer of fat under their skin that acts as insulation, helping them regulate body temperature in different environments.
-They are excellent at camouflaging themselves in their surroundings, blending in with vegetation to avoid detection by predators.
-Capybaras have a well-developed memory and can remember the location of food sources and safe areas.
-They have long, sturdy claws on their front feet that aid in digging burrows and manipulating food.
-Capybaras have a broad and flat muzzle, which allows them to graze efficiently on grass and other low-lying vegetation.
-They have a strong bite force and can crush hard-shelled fruits and vegetation.
-Capybaras have large, sensitive nostrils that can be closed voluntarily to prevent water from entering while they are submerged.
-They have a keen sense of touch and use their whiskers to detect vibrations and movements in the water.
-Capybaras are known to engage in "riding behavior," where smaller animals like birds or turtles may perch on their backs.
-They have a unique mating behavior called "coprophagy," where males eat the feces of females as a form of courtship.
-Capybaras have a long and slender body, which allows them to move through dense vegetation with ease.
-They have a strong maternal instinct, and mothers will defend their young fiercely against any potential threats.
-Capybaras have a wide range of facial expressions, including raising their eyebrows and flattening their ears to communicate different emotions.
-They have a highly developed sense of touch in their snout and lips, which helps them identify and select their food.
